94fcb3bd0639b87bb7692be329e5811eda64f060
John Hanks <jhanks@jumptrading.com>
2026-01-14 10:17:30 -0500

    Move _shutdown_x11_forward(step) inside the child/fork
    This prevents a potential deadlock in glibc if drop_privileges() is
    called while other threads are blocked and the setxid signal can't be
    processed.
    
    Ticket: 24439
    Changelog: Fix potential glibc deadlock when tearing down the extern
     step when x11 forwarding is enabled.

--- src/slurmd/slurmstepd/mgr.c
+++ src/slurmd/slurmstepd/mgr.c
@@ -1139,13 +1139,10 @@ static void _x11_signal_handler(conmgr_callback_args_t conmgr_args, void *ignore
 
 	debug("Terminate signal (SIGTERM) received");
 
-	if (!_need_join_container()) {
-		_shutdown_x11_forward();
-		return;
-	}
 	if ((cpid = fork()) == 0) {
-		if (namespace_g_join(&step->step_id, step->uid, false) !=
-		    SLURM_SUCCESS) {
+		if ((_need_join_container()) &&
+		    (namespace_g_join(&step->step_id, step->uid, false) !=
+		     SLURM_SUCCESS)) {
 			error("%s: cannot join container",
 			      __func__);
 			_exit(1);
